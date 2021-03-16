086447ec731a6c9fccc4555ccc164716cad33a5a
Jacky Shi <jacky@am-tech.biz>
2021-03-16 17:51:27 -0400

    Integrate OpenWeather API

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,45 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Jacky on 3/16/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=1850553fd20fa11ee69562138f53f8c9&units=imperial"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        // 1. Create a URL
+        if let url = URL(string: urlString) {
+            
+            // 2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            // 3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
+}
